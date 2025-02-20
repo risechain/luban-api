d06a4b2e8d0c54bb69423b3c1105e297746f0f63
i1i1 <vanyarybin1@live.ru>
2025-02-20 17:33:51 +0300

    Add error context to client

--- client/client.go
+++ client/client.go
@@ -24,7 +24,7 @@ type Client struct {
 func NewClient(server string, key *ecdsa.PrivateKey, opts ...internal.ClientOption) (*Client, error) {
 	cl, err := internal.NewClientWithResponses(server, opts...)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Failed to make preconf http client: %w", err)
 	}
 	client := Client{ClientWithResponses: cl, key: key}
 	return &client, nil
@@ -33,10 +33,10 @@ func NewClient(server string, key *ecdsa.PrivateKey, opts ...internal.ClientOpti
 func (cl *Client) GetSlots(ctx context.Context) ([]types.SlotInfo, error) {
 	resp, err := cl.ClientWithResponses.GetSlotsWithResponse(ctx)
 	if err != nil {
-		return []types.SlotInfo{}, err
+		return []types.SlotInfo{}, fmt.Errorf("Http request for getting slots failed: %w", err)
 	}
 	if resp.JSON200 == nil {
-		return []types.SlotInfo{}, fmt.Errorf("GetSlots return code %v", resp.Status())
+		return []types.SlotInfo{}, fmt.Errorf("GetSlots return code %v: %s", resp.Status(), string(resp.Body))
 	}
 	return *resp.JSON200, nil
 }
@@ -45,10 +45,10 @@ func (cl *Client) GetSlots(ctx context.Context) ([]types.SlotInfo, error) {
 func (cl *Client) GetPreconfFee(ctx context.Context, slot uint64) (uint64, uint64, error) {
 	resp, err := cl.ClientWithResponses.GetFeeWithResponse(ctx, slot)
 	if err != nil {
-		return 0, 0, err
+		return 0, 0, fmt.Errorf("Http request for getting preconf fee failed: %w", err)
 	}
 	if resp.JSON200 == nil {
-		return 0, 0, fmt.Errorf("GetPreconfFee return code %v and error `%v`", resp.Status(), resp.JSON500)
+		return 0, 0, fmt.Errorf("GetPreconfFee return code %v and error `%v`", resp.Status(), string(resp.Body))
 	}
 	return resp.JSON200.GasFee, resp.JSON200.BlobGasFee, nil
 }
@@ -87,7 +87,7 @@ func (cl *Client) ReserveBlockspace(
 func (cl *Client) signSubmitTx(reqId uuid.UUID, tx *types.Transaction) (string, error) {
 	signature, err := crypto.Sign(types.SubmitTxDigest(reqId, tx).Bytes(), cl.key)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Failed to sign preconf tx: %w", err)
 	}
 	return fmt.Sprintf("0x%s", hex.EncodeToString(signature)), nil
 }
@@ -108,10 +108,10 @@ func (cl *Client) SubmitTransaction(ctx context.Context, reqId uuid.UUID, tx *ty
 	}
 	resp, err := cl.SubmitTransactionWithResponse(ctx, &params, req)
 	if err != nil {
-		return err
+		return fmt.Errorf("SubmitTransaction http request failed: %w", err)
 	}
 	if resp.JSON200 == nil {
-		return fmt.Errorf("SubmitTransaction return code %v", resp.Status())
+		return fmt.Errorf("SubmitTransaction return code %v: %s", resp.Status(), string(resp.Body))
 	}
 	return err
 }
