1ee61bb88bf86ee5ad67e0f3da72c08845147d0b
i1i1 <vanyarybin1@live.ru>
2025-02-21 15:44:57 +0300

    Use env variable for prefunded key

--- txmgr/txmgr_test.go
+++ txmgr/txmgr_test.go
@@ -21,8 +21,12 @@ import (
 )
 
 func TestTxmgr(t *testing.T) {
+	keyStr, notSet := os.Lookupenv("TEST_LUBAN_KEY")
+	if notSet {
+		return
+	}
+	key, _ := crypto.HexToECDSA(keyStr)
 	l := testlog.Logger(t, log.LevelTrace)
-	key, _ := crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f292")
 	chainId := big.NewInt(7028081469)
 	addr := crypto.PubkeyToAddress(key.PublicKey)
 	cfg := &txmgr.Config{
